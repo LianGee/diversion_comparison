7084f423071479a06fdd3351ada009d4f58d3828
LianGee <bo_bo0425@126.com>
2017-06-29 16:16:03 +0800

    test case

--- case/test.py
+++ case/test.py
@@ -1,14 +1,11 @@
 from cap.parse_cap import PcapService
 
 
-def create_test_case(cap_file='unified_cache_server.cap', step=100, r=0):
+def create_test_case(cap_file='unified_cache_server.cap', step=100, r=0, f=None):
     ps = PcapService(cap_file)
-    post_data = ps.get_udp_data(step, r)
+    post_data = ps.get_udp_data(step, r, f)
     return post_data
 
-if __name__ == '__main__':
-    tests = create_test_case('unified_cache_server.cap', 100, 2)
-    for t in tests:
-        print t.id, t.seq
-        print t.request
+
+
 
--- case/test_case.py
+++ case/test_case.py
@@ -1,3 +1,6 @@
+from protobuf.cs_pb2 import RespBody
+
+
 test_time_out = []  # 2
 err_code_99 = []  # 3
 err_code_1 = []  # 4
@@ -8,8 +11,9 @@ failed = []
 
 
 class TestCase:
-    def __init__(self, id=0, seq=0, request=None, response1=None, response2=None):
+    def __init__(self, id=0, data=None, seq=0, request=None, response1=None, response2=None):
         self.id = id
+        self.data = data
         self.seq = seq
         self.request = request
         self.response1 = response1
@@ -17,16 +21,50 @@ class TestCase:
         self.result = 0  # 0: error bug, 1 pass, 2 test time out, 3 error_code_99, 4 error_code_1, 5 online time out
 
     def check_response(self):
-        if 'err_code: 99' in self.response1 or 'flag: 99' in self.response1:
-            test_time_out.append(self.id)
+        if self.response2 in self.response1:
+            success.append(self.id)
+            self.result = 1
+        elif 'err_code: 99' in self.response1 or 'flag: 99' in self.response1:
+            err_code_99.append(self.id)
             failed.append(self.id)
+            self.result = 3
         elif 'err_code: 1' in self.response1:
             err_code_1.append(self.id)
             failed.append(self.id)
+            self.result = 4
+        elif len(self.response1) == 0:
+            test_time_out.append(self.id)
+            failed.append(self.id)
+            self.result = 2
         elif 'err_code: 99' in self.response2 or 'flag: 99' in self.response2\
                 or len(self.response2 == 0):
             online_time_out.append(self.id)
             success.append(self.id)
+            self.result = 5
         else:
             err.append(self.id)
             failed.append(self.id)
+            self.result = 0
+
+    @staticmethod
+    def set_response(reply):
+        res = RespBody()
+        data = reply[24:]
+        print 'response length:', len(data)
+        try:
+            res.ParseFromString(data)
+        except Exception, e:
+            res = ''
+            print e, 'response decode failed!!!!'
+        return str(res)
+
+    @staticmethod
+    def show_reslut():
+        print 'failed %s' % len(failed), err
+        print 'success %s' % len(success), success
+        print 'test time out %s' % len(test_time_out), test_time_out
+        print 'err_code_99 %s' % len(err_code_99), err_code_99
+        print 'err_code_1 %s' % len(err_code_1), err_code_1
+        print 'online time out %s' % len(online_time_out), online_time_out
+        print 'err %s' % len(err), err
+
