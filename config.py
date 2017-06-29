79d9f7397652ce4a982d8392b4f786ec8d6b89cd
LianGee <bo_bo0425@126.com>
2017-06-29 16:16:16 +0800

    config

---
+++ config.py
@@ -0,0 +1,9 @@
+class __Config:
+    def __init__(self):
+        self.step = 10000
+        self.r = 0
+        self.file = './dump_files/unified_cache_server.cap'
+        self.test_ip = '10.236.21.144'
+        self.test_port = 9901
+        self.online_ip = '10.236.21.144'
+        self.online_port = 9901
